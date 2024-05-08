54c5deb11eab0b9bb3ec175c3b56a97107a263c1
mskrasnov <linuxoid85@gmail.com>
2024-05-08 20:26:23 +0300

    [refact] Упроситл получение параметров командной строки и конфига

--- src/main.rs
+++ src/main.rs
@@ -13,38 +13,14 @@ use conf::Conf;
 use formatter::FmtMode;
 use weather::Weather;
 
-fn get_data<'a>(cl: &'a Cli, cn: &'a Conf) -> Result<Weather<'a>, String> {
-    // Oh shit, I'm sorry. But it works
-
-    let loc = if let Some(loc) = &cl.location {
-        loc
+fn get_data(cli: Option<String>, conf: Option<String>) -> Option<String> {
+    if cli.is_none() && conf.is_none() {
+        None
+    } else if cli.is_some() {
+        cli
     } else {
-        if let Some(loc) = &cn.location {
-            loc
-        } else {
-            return Err("You must explicitly specify the location in \
-                        the config (~/.config/swf.toml) or in the \
-                        --location=... key"
-                .to_string());
-        }
-    };
-
-    let key = if let Some(key) = &cl.api_key {
-        key
-    } else {
-        if let Some(key) = &cn.api_key {
-            key
-        } else {
-            return Err("You must explicitly specify the API Key in \
-                        the config (~/.config/swf.toml) or in the \
-                        --api-key=... key"
-                .to_string());
-        }
-    };
-
-    let weather = Weather::new(loc, key);
-
-    Ok(weather)
+        conf
+    }
 }
 
 #[tokio::main]
@@ -63,7 +39,31 @@ async fn main() -> anyhow::Result<()> {
     };
     let conf = Conf::parse("./swf.toml").await?;
 
-    let weather = get_data(&cli, &conf).unwrap();
+    let location = match get_data(cli.location, conf.location) {
+        Some(loc) => loc,
+        None => {
+            eprintln!(
+                "You must explicitly specify the location in \
+                the config (~/.config/swf.toml) or in the \
+                --location=... key."
+            );
+            exit(1)
+        }
+    };
+
+    let key = match get_data(cli.api_key, conf.api_key) {
+        Some(key) => key,
+        None => {
+            eprintln!(
+                "You must explicitly specify the API key in \
+                the config (~/.config/swf.toml) or in the \
+                --api-key=... key."
+            );
+            exit(1)
+        }
+    };
+
+    let weather = Weather::new(&location, &key);
     let data = weather.get().await?;
 
     fmt_mode.print(&data, weather.units.clone());
